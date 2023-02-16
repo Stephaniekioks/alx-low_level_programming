9a2a2fee6ef3f1c6c8e6b310468d6368153c87d7
stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-16 14:28:14 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints "and that peice of art is useful" - Dora korpar, 2015-10-19",
+ * followed by a newline, to standard error.
+ * Return: Always 1.
+ */
+
+int main(void)
+
+{
+	write(2, "and taht peice of art is useful\" - Dora korpar,2015-10-19\n",59);
+	return (1);
+}
