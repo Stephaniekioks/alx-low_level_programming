b1886ed25cf679b838f9d454682bf0992df5b311
stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-16 12:09:13 -0800

    code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
- * main - Printing out a line
- * Return: 0
+ * main - Prints \"programming is like building a multilingual puzzle
+ *go to new line after printing
+ *use puts
+ *Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
