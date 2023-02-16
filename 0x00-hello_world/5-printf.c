fac05e7e9b2d4b94a30bbd1ecda630e24488d940
stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-16 13:15:06 -0800

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Printing with printf
- * Return: 0
- */
-int main(void)
-{
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
-}
+ * main - printing with printf
+ * Returm: 0
+ * /
+ int main(void)
+ {
+ printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
+ }
