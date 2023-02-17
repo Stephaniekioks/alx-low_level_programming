d5fd3d86908453f530af6d5890fc58785afcc93c
stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-17 10:34:24 -0800

    base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,13 +4,13 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int d;
 	char low;
-	for (d ='0'; d <= '9'; d++)
+	for (d = '0'; d <= '9'; d++)
 		putchar(d);
 	for (low = 'a'; low <= 'f'; low++)
 		putchar(low);
