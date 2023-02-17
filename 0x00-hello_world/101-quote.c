cfed7351ce39d016112606dced8ac852464a3946
stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-17 03:42:34 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - prints "and that peice of art is useful" - Dora korpar, 2015-10-19",
- * followed by a newline, to standard error.
- * Return: Always 1.
+ * main - prints without using printf or puts
+ * Return: 0
  */
 
 int main(void)
 
 {
-	write(2, "and taht peice of art is useful\" - Dora korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
