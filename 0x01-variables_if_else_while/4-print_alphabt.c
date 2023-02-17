b7d932967f92e0da40aedc991a20fe957d0f800c
stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-17 12:28:19 -0800

    al

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,17 @@
  */
 int main(void)
 {
-        char low, e, q;
-        e = 'e';
-        q = 'q';
-        for (low = 'a'; low <= 'z'; low++)
-        {
-        if (low != e && low != q)
-        putchar(low);
-        }
-        putchar('\n');
-        return (0);
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+	putchat('/n');
+
+	return (0);
 }
