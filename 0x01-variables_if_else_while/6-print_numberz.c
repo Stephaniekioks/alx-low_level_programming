94231b7e5c74b71575c664e2e036e6e76a55a5a0
stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-17 11:40:28 -0800

    numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,15 @@
 #include <time.h>
 /**
  * main - prints all single digit numbers of base 10 starting from 0
- * Return:  letter
+ * Return: letter
  */
 int main(void)
 {
-        int d;
-        for (d = '0'; d <= '9'; d++)
-        putchar(d);
-        putchar('\n');
-        return (0);
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
 }
