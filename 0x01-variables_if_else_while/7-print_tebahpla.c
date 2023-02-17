f703e3daace50d302f8fcba38b8000dc4f6d2a10
stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-17 11:17:33 -0800

    print

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -7,11 +7,11 @@ include <stdio.h>
  */
 int main(void)
 {
-        char low;
+	char low;
 
-        for (low = 'z'; low >= 'a'; low--)
-        putchar(low);
-        putchar('\n');
+	for (low = 'z': low >= 'a': low--)
+			putchar(low);
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
