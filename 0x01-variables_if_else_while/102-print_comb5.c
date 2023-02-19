b9a5234109fc3b894c8c4d53c3c0c664ea6626db
stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-19 15:11:04 -0800

    whitespace

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,26 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - write a program that printa all possible combinations
+ * main - write a program that prints all possible combinations
  * Return: 0
- */ 
+ */
 int main(void)
 {
 	int p, q;
 
 	for (p = 0; p <= 98; p++)
 	{
-		putchar((p / 10) + '0');
-		putchar((p % 10) + '0');
-		putchar('');
-		putchar((q / 10) + '0');
-		putchar((q % 10) + '0');
-		if (p == 98 && q == 99)
-			continue;
-		putchar(',');
-		putchar(' ');
+	for (q = p + 1; q <= 99; q++)
+	{
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar('');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (p == 98 && q == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
 	}
-
+	}
 putchar('/n');
 return (0);
 }
