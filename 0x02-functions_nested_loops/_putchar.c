8fda6e6733def02d3014c53ee474675b9eab82ac
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-21 09:48:02 -0800

    push

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** 
+ * main - Entry point
+ * print _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char str[] = "_putchar\n";
+        int ch;
+
+        for (ch = 0; ch <= 8; ch++)
+        {
+        putchar(str[ch]);
+        }
+        return (0);
+}
