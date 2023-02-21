7bcdb66613bda17abf30cb7ce02b985533e534de
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-21 14:45:44 -0800

    m

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print _last_digit - prints the last digit of a number
+ * print_last_digit - prints the last digit of a number
  *@n: the int to extract the last digit
  * Return: value of the last digit
  */
