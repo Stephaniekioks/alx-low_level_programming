8ddf8957bbc0d09849ca5e19f94e577a8d070ed2
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-22 01:41:33 -0800

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,8 +20,8 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
-			_putchar(k + '0';
-			} else if (k >=10)
+			_putchar(k + '0');
+			} else if (k >= 10)
 			{
 			_putchar(',');
 			_putchar(' ');
