d444add92ff2ad19754f1717358174405658be62
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-02-23 11:26:58 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,10 +14,10 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		} else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
@@ -26,7 +26,7 @@ int main(void)
 			printf("%d", i);
 		} else
 		{
-			printf(" %d", i);
+			printf("%d", i);
 		}
 	}
 	printf("\n");
