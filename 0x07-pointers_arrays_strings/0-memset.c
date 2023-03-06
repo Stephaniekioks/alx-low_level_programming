8dfc0914fb8c3e3711491e41880e1b775fd1032d
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-03-06 12:06:47 -0800

    memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i;
 
-	for (i = 0; n > 0; 1++)
+	for (i = 0; i < n; i++)
 	{
-		s[1] = b;
+		s[i] = b;
 	}
 
 	return (s);
