c39ec6c8db3ec4daea47b3603e341a1cb711220c
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-04-04 03:46:58 -0700

    listint_len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h: linked list of type listint_t
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
