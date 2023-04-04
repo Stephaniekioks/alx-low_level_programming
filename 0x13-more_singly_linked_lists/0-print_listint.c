233df6e65ad9a4fd12e7287e6042e0641906c05c
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-04-04 02:59:02 -0700

    listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,17 @@
  *
  * Return: the number of nodes
  */
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
-	size_t int = 0;
+	size_t count;
+
+	count = 0;
 
 	while (h)
 	{
 		printf("%d/n", h->n);
-		int++;
 		h = h->next;
+		count++;
 	}
-	return (int);
+	return (count);
 }
