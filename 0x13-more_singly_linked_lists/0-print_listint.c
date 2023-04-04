81f7e295daa7fdd9415266ed3e056d368bc89b66
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-04-04 03:04:29 -0700

    listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,15 +10,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
-
-	count = 0;
+	size_t num0 = 0;
 
 	while (h)
 	{
 		printf("%d/n", h->n);
 		h = h->next;
-		count++;
+		num0++;
 	}
-	return (count);
+	return (num0);
 }
