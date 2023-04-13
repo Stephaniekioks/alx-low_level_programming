2aacb6701e1abb81c64252d879a9c72a92131ad5
Stephaniekioks <kiokosteff.ks@gmail.com>
2023-04-13 05:44:40 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,18 +8,18 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int i, j, text = 0;
+	int i, j, x = 0;
 
 	if (filename = NULL)
 		return(-1);
 
 	if (text_content != NULL)
 	{
-		for (text = 0; text_content[text];)
-			text++;
+		for (x = 0; text_content[x];)
+			x++;
 	}
 	i = open(filename, O_WRONLY | O_APPEND);
-	j = write(i, text_content, text);
+	j = write(i, text_content, x);
 
 	if (i == -1 || j == -1)
 		return (-1);
